fda9301efb649335aae72c892288409ea6b07565
RoboZe-01 <roboticsxzero@gmail.com>
2024-09-07 23:46:27 +0530

    Add files via upload

---
+++ Arduino-Uno Practice/Gas_sensor_Project.ino
@@ -0,0 +1,20 @@
+#include <MQ2.h>
+
+#include <LCD_I2C.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+
+
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
