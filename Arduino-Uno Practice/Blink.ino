aecbe4f7e793280354858b6291ac49ae0f0d064d
RoboZe-01 <roboticsxzero@gmail.com>
2024-09-07 23:45:55 +0530

    Add files via upload

---
+++ Arduino-Uno Practice/Blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(7, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (7,HIGH );      // It will turn on the led 
+delay(1000);
+digitalWrite (7,LOW);    // LOW means it will turn of the LED
+delay(1000);              // Delay means it will stop for given time 
+}
